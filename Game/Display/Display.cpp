4ba8ee9c08318c355d002495a9aa26c6e1018396
Vincent <vincent.lemesle@epitech.eu>
2019-05-09 16:38:04 +0100

    [ADD] -> Display class

---
+++ Game/Display/Display.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+#include "DisplayExceptions.hpp"
+#include "Display.hpp"
+
+Display::Display()
+{
+    _device = irr::createDevice(irr::video::EDT_OPENGL,
+    irr::core::dimension2d<irr::u32>(640, 480));
+    if (!_device)
+        throw DeviceCreationError();
+    _device->setWindowCaption(windowName);
+    _gui = _device->getGUIEnvironment();
+    _driver = _device->getVideoDriver();
+    _scenes = _device->getSceneManager();
+}
+
+void    Display::addNewMesh(const char *meshPath)
+{
+    auto    newMesh = _scenes->getMesh(meshPath);
+    if (!newMesh)
+        throw MeshCreationError();
+    _meshs.push_back(newMesh);
+}
+
+void    Display::addNewMeshScene(const char *scenePath)
+{
+    auto    newScene = _scenes->addAnimatedMeshSceneNode(_meshs.back());
+    if (!newScene)
+        throw MeshSceneCreationError();
+    newScene->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+    newScene->setMD2Animation(irr::scene::EMAT_STAND);
+    newScene->setMaterialTexture( 0, _driver->getTexture(scenePath));
+    _meshsScene.push_back(newScene);
+}
+
+void    Display::addNewAnimation(const char *meshPath, const char *scenePath)
+{
+    addNewMesh(meshPath);
+    addNewMeshScene(scenePath);
+}
+
+bool    Display::isRunning() const
+{
+    return _device->run();
+}
+
+void    Display::setGuiMessage(const wchar_t *message)
+{
+    _gui->addStaticText(message, irr::core::rect<irr::s32>(10,10,260,22), true);
+}
+
+void    Display::setCameraScene()
+{
+    _scenes->addCameraSceneNode(0, irr::core::vector3df(0,30,-40), irr::core::vector3df(0,5,0));
+}
+
+void    Display::draw()
+{
+    _driver->beginScene(true, true, irr::video::SColor(255,100,101,140));
+    _scenes->drawAll();
+    _gui->drawAll();
+    _driver->endScene();
+}
--- irrlichtTest/test.cpp
+++ irrlichtTest/test.cpp
@@ -2,52 +2,44 @@
 
 #include "Events.hpp"
 
-using namespace irr;
-
-using namespace core;
-using namespace scene;
-using namespace video;
-using namespace io;
-using namespace gui;
-
 int main()
 {
-    IrrlichtDevice *device =
-            createDevice(video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
+    irr::IrrlichtDevice *device =
+            irr::createDevice(irr::video::EDT_SOFTWARE, irr::core::dimension2d<irr::u32>(640, 480), 16,
                           false, false, false, 0);
     if (!device)
         return 1;
     device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
-    IVideoDriver* driver = device->getVideoDriver();
-    ISceneManager* smgr = device->getSceneManager();
-    IGUIEnvironment* guienv = device->getGUIEnvironment();
+    irr::video::IVideoDriver* driver = device->getVideoDriver();
+    irr::scene::ISceneManager* smgr = device->getSceneManager();
+    irr::gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
     guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
-                          rect<s32>(10,10,260,22), true);
-    IAnimatedMesh* mesh = smgr->getMesh("./../LibIrrlicht/media/faerie.md2");
-    IAnimatedMesh* mesh2 = smgr->getMesh("./../LibIrrlicht/media/sydney.md2");
+                          irr::core::rect<irr::s32>(10,10,260,22), true);
+    irr::scene::IAnimatedMesh* mesh = smgr->getMesh("./../LibIrrlicht/media/faerie.md2");
+    irr::scene::IAnimatedMesh* mesh2 = smgr->getMesh("./../LibIrrlicht/media/sydney.md2");
     if (!mesh)
     {
         device->drop();
         return 1;
     }
-    IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
+    irr::scene::IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
     if (node)
     {
-        node->setMaterialFlag(EMF_LIGHTING, false);
-        node->setMD2Animation(scene::EMAT_STAND);
+        node->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+        node->setMD2Animation(irr::scene::EMAT_STAND);
         node->setMaterialTexture( 0, driver->getTexture("./../LibIrrlicht/media/faerie2.bmp") );
     }
-    IAnimatedMeshSceneNode* node2 = smgr->addAnimatedMeshSceneNode( mesh2 );
+    irr::scene::IAnimatedMeshSceneNode* node2 = smgr->addAnimatedMeshSceneNode( mesh2 );
     if (node2)
     {
-        node2->setMaterialFlag(EMF_LIGHTING, false);
-        node2->setMD2Animation(scene::EMAT_STAND);
+        node2->setMaterialFlag(irr::video::EMF_LIGHTING, false);
+        node2->setMD2Animation(irr::scene::EMAT_STAND);
         node2->setMaterialTexture( 0, driver->getTexture("./../LibIrrlicht/media/sydney.bmp") );
     }
-    smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+    smgr->addCameraSceneNode(0, irr::core::vector3df(0,30,-40), irr::core::vector3df(0,5,0));
     while(device->run())
     {
-        driver->beginScene(true, true, SColor(255,100,101,140));
+        driver->beginScene(true, true, irr::video::SColor(255,100,101,140));
 
         smgr->drawAll();
         guienv->drawAll();
