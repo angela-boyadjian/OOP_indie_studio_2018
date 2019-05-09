ff9f52243bdfa33ad8e2cfa387c7234631fad5d3
angela-boyadjian <boyadjian.angela@gmail.com>
2019-05-09 16:13:57 +0200

    [EDIT] Test.cpp -> Now displays bomberman

--- irrlichtTest/test.cpp
+++ irrlichtTest/test.cpp
@@ -21,38 +21,29 @@ int main()
     IGUIEnvironment* guienv = device->getGUIEnvironment();
     guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
                           rect<s32>(10,10,260,22), true);
-    IAnimatedMesh* mesh = smgr->getMesh("./../LibIrrlicht/media/faerie.md2");
-    IAnimatedMesh* mesh2 = smgr->getMesh("./../LibIrrlicht/media/sydney.md2");
-    if (!mesh)
-    {
+    IAnimatedMesh* mesh = smgr->getMesh("../resources/models/Character/Bomberman.MD3");
+    if (!mesh) {
         device->drop();
         return 1;
     }
-    IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
-    if (node)
-    {
+    IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode(mesh);
+    if (node) {
         node->setMaterialFlag(EMF_LIGHTING, false);
+        node->setScale(vector3df(6, 6, 6));
+        node->setRotation(vector3df(0, 0, 0));
+        node->setAnimationSpeed(30);
+        node->setLoopMode(true);
         node->setMD2Animation(scene::EMAT_STAND);
-        node->setMaterialTexture( 0, driver->getTexture("./../LibIrrlicht/media/faerie2.bmp") );
-    }
-    IAnimatedMeshSceneNode* node2 = smgr->addAnimatedMeshSceneNode( mesh2 );
-    if (node2)
-    {
-        node2->setMaterialFlag(EMF_LIGHTING, false);
-        node2->setMD2Animation(scene::EMAT_STAND);
-        node2->setMaterialTexture( 0, driver->getTexture("./../LibIrrlicht/media/sydney.bmp") );
+        node->setMaterialTexture(0, driver->getTexture("../resources/models/Character/RedBombermanTextures.png"));
+        node->setFrameLoop(27, 76);
     }
     smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
-    while(device->run())
-    {
+    while(device->run()) {
         driver->beginScene(true, true, SColor(255,100,101,140));
-
         smgr->drawAll();
         guienv->drawAll();
-
         driver->endScene();
     }
     device->drop();
-
     return 0;
 }
