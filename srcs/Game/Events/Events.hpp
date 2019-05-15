6f39b6d2c77d10d2913de7598e78780ef733a7b7
Vincent <vincent.lemesle@epitech.eu>
2019-05-15 16:48:10 +0200

    [EDIT] -> Event class is now fully templated

--- srcs/Game/Events/Events.hpp
+++ srcs/Game/Events/Events.hpp
@@ -4,20 +4,23 @@
 #include <driverChoice.h>
 
 static const size_t KEY_COUNT = irr::KEY_KEY_CODES_COUNT;
+static const size_t INPUT_EVENT = irr::EET_KEY_INPUT_EVENT;
 
 template <typename T>
 class Events : public T {
 public:
+    using index = irr::u32;
     Events() {
-        for (irr::u32 i = 0; i < irr::KEY_KEY_CODES_COUNT; ++i)
+        for (index i = 0; i < KEY_COUNT; ++i)
             _keyIsPressed[i] = false;
     }
 
-    template <typename U> // U ->irr::SEvent
-    bool    OnEvent(const U& event) {
-        if (event.EventType == irr::EET_KEY_INPUT_EVENT)
-            _keyIsPressed[event.KeyInput.Key] = event.KeyInput.PressedDown;
-        return false;
+    template <typename U> // U ->irr::SEvent::EventType
+    bool    isInputEvent(const U &eventType) { return eventType == INPUT_EVENT; }
+
+    template <typename U> // U -> irr::SEvent::KeyInput::Key
+    void    OnEvent(const U& key) {
+        _keyIsPressed[key] = true;
     }
 
     template <typename V> // V -> irr::EKEY_CODE
