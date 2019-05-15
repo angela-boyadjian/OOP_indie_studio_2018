f5f745d30e54e125246b0860bfa1c2a02f745a68
ClementDBS <clement.dubois@epitech.eu>
2019-05-15 12:11:42 +0200

    [ADD] start to display button quit

--- srcs/Core/Bomberman.cpp
+++ srcs/Core/Bomberman.cpp
@@ -22,18 +22,18 @@ void core::Bomberman::run()
     while (_display.isRunning()) {
         for (auto const &player : _players)
             player->displayPlayer();
-        _menu->displayMenu();
-        _display.draw();
+//        _menu->displayMenu();
+       _display.draw();
     }
 }
 
 void core::Bomberman::loadGame()
 {
     _display.setCameraScene();
-//    _display.setGuiMessage(L"hello");
+    _display.setGuiMessage(L"hello");
     _players.push_back(std::make_unique<Player>(ACharacter::Color::BLACK,
         std::make_tuple(1, 2), _display));
     for (auto const &player : _players)
         player->loadPlayer();
-    _menu->loadMenu();
+//    _menu->loadMenu();
 }
