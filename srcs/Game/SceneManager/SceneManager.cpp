4354d276825559819448bdb8a7f8e7cf7828210a
ClementDBS <clement.dubois@epitech.eu>
2019-05-20 16:38:44 +0200

    [MERGE]

---
+++ srcs/Game/SceneManager/SceneManager.cpp
@@ -0,0 +1,17 @@
+//
+// Created by dclement on 5/20/19.
+//
+
+#include "SceneManager.hpp"
+
+SceneManager::SceneManager(IDisplay::Device const &device) : _device(device), _smgr(_device->getSceneManager())
+{
+}
+
+void SceneManager::createMenuScene()
+{
+}
+
+void SceneManager::addScene()
+{
+}
