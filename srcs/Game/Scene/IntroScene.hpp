7c688dc8fde064b52b1528daf5a2004f08cc39fa
angela-boyadjian <boyadjian.angela@gmail.com>
2019-06-14 21:22:36 +0200

    [ADD] Animation for press start

--- srcs/Game/Scene/IntroScene.hpp
+++ srcs/Game/Scene/IntroScene.hpp
@@ -10,13 +10,18 @@
 #include <memory>
 #include <vector>
 #include <array>
+#include <chrono>
 
 #include "IScene.hpp"
+#include "Events.hpp"
 
 class IntroScene : public IScene {
 public:
-    IntroScene(std::shared_ptr<irr::IrrlichtDevice>, irr::scene::ISceneNode *,
-        std::string const &, const irr::core::dimension2du &, irr::video::IVideoDriver *);
+    using Timer = std::chrono::time_point<std::chrono::steady_clock,
+        std::chrono::nanoseconds>;
+    
+    IntroScene(std::shared_ptr<IDisplay>, irr::scene::ISceneNode *,
+        std::string const &, std::shared_ptr<Events>);
     ~IntroScene() = default;
 
 public:
@@ -26,13 +31,21 @@ public:
     std::string getName() final;
 
 private:
+    void loadImg() noexcept;
+    void updateImg();
+    std::size_t getElapsedTime() const;
+
+private:
+    Timer   _clock;
     std::shared_ptr<irr::scene::ISceneNode> _master;
     irr::scene::ISceneManager *_manager;
     std::string _name;
     bool _is_load;
     std::vector<std::shared_ptr<irr::scene::ISceneNode>> _cubes;
-    std::array<irr::gui::IGUIImage *, 2> _images;
-    std::array<irr::video::ITexture *, 2> _textures;
+    std::array<irr::gui::IGUIImage *, 2>    _images;
+    std::array<irr::video::ITexture *, 2>   _textures;
     const irr::core::dimension2du& _win_size;
     std::shared_ptr<irr::IrrlichtDevice> _device;
+    std::shared_ptr<Events> _event;
+    bool _isVisible;
 };
