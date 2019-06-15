cae69477156d01666eaf8ee4b464f4866d6a69c8
Basile <basile.lamarque@epitech.eu>
2019-06-15 09:11:16 +0200

    [ADD]: Struct wich pass info scene to scene
    	new file:   srcs/Game/Scene/SceneInfo.cpp
    	new file:   srcs/Game/Scene/SceneInfo.hpp

---
+++ srcs/Game/Scene/SceneInfo.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2019
+** SceneInfo.cpp 
+** File description:
+** basile.lamarque@epitech.eu
+*/
+
+#include "SceneInfo.hpp"
---
+++ srcs/Game/Scene/SceneInfo.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** SceneInfo.hpp 
+** File description:
+** basile.lamarque@epitech.eu
+*/
+
+
+#pragma once
+
+#include "IMap.hpp"
+#include "ACharacter.hpp"
+
+struct SceneInfo {
+        SceneInfo(const std::string dest) : _dest(dest) {};
+
+        std::string _dest;
+        std::shared_ptr<IMap> _map;
+        std::vector<ACharacter> _characters;
+};
+
+
+
