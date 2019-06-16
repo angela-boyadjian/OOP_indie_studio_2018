8b3df8a21ae023060c97a5a610f4306a815e41ee
angela-boyadjian <boyadjian.angela@gmail.com>
2019-06-16 14:23:10 +0200

    [EDIT] ILoad

--- srcs/Game/Save/ILoad.hpp
+++ srcs/Game/Save/ILoad.hpp
@@ -19,10 +19,17 @@ public:
     virtual std::unique_ptr<AGame> loadGame() = 0;
 
 private:
-    virtual void addBot(std::string const &) = 0;
-    virtual void addPlayer(std::string const &) = 0;
-    virtual float convertVal(std::string &, int &) = 0;
-    virtual ACharacter::Color getSkin(std::string const &) = 0;
-    virtual void printPos(ACharacter::MapPos const &) const = 0;
-    virtual ACharacter::MapPos  getMapPos(std::string const &, int) = 0;
+    virtual void                    addBot(std::string const &) = 0;
+    virtual void                    getInfo(std::string const &) = 0;
+    virtual irr::core::vector3df    getSize(std::string const &) = 0;
+    virtual std::string const       getPath(std::string const &) = 0;
+    virtual ACharacter::Color       getSkin(std::string const &) = 0;
+    virtual void                    addPlayer(std::string const &) = 0;
+    virtual void                    getMapWall(std::string const &) = 0;
+    virtual float                   convertVal(std::string &, int &) = 0;
+    virtual void                    getRulesWall(std::string const &) = 0;
+    virtual void                    getRulesGround(std::string const &) = 0;
+    virtual SpriteInfo              &getSpriteInfo(std::string const &) = 0;
+    virtual ACharacter::MapPos      getMapPos(std::string const &, int) = 0;
+    virtual void                    printPos(ACharacter::MapPos const &) const = 0;
 };
